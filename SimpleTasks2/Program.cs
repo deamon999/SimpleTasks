6f6f16cc26f8712ad41484410d3ea6ba00633ca9
Dmytro Ileiko <Dmytro.Ileiko@ist.com>
2021-01-02 11:21:27 +0200

    Added Task 3

--- SimpleTasks2/Program.cs
+++ SimpleTasks2/Program.cs
@@ -6,7 +6,27 @@ namespace SimpleTasks2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lineA, lineB, lineC;
+            Console.WriteLine("Please enter width of first line:");
+            string lineAStr = Console.ReadLine();
+            int.TryParse(lineAStr, out lineA);
+
+            Console.WriteLine("Please enter width of first line:");
+            string lineBStr = Console.ReadLine();
+            int.TryParse(lineBStr, out lineB);
+
+            Console.WriteLine("Please enter width of first line:");
+            string lineCStr = Console.ReadLine();
+            int.TryParse(lineCStr, out lineC);
+
+            double square = triangelSquare(lineA, lineB, lineC);
+            Console.WriteLine("Triangel Square: {0}", square);
+        }
+
+        static double triangelSquare(int lineA, int lineB, int lineC)
+        {
+            double parametr = (lineA + lineB + lineC) / 2;
+            return Math.Sqrt(parametr * (parametr - lineA) * (parametr - lineB) * (parametr - lineC));
         }
     }
 }
---
+++ SimpleTasks3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleTasks3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter circle radius");
+            int radius;
+            int.TryParse(Console.ReadLine(), out radius);
+
+            Console.WriteLine($"Circle length is: {Math.PI * radius * 2}");
+        }
+    }
+}
