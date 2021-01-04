56770d503c5cce859be7305455cfa48e94f7d47d
Dmytro Ileiko <Dmytro.Ileiko@ist.com>
2021-01-04 16:21:36 +0200

    Added 3 tasks, task 12 added initial structure and this one will be final here.

---
+++ SimpleTasks10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleTasks10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filePath = @"D:\test_file.txt";
+
+            Console.WriteLine("Enter text yoou want to save, and press 'Enter':");
+            string text = Console.ReadLine();
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                }
+                Console.WriteLine("Write operation is done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Reading from file text you have entered:");
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SimpleTasks9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleTasks9
+{
+    class Program
+    {
+        static readonly string driveName = "D:\\";
+        static void Main(string[] args)
+        {
+
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in driveInfos)
+            {
+                Console.WriteLine(driveInfo.Name);
+                if (driveInfo.Name.StartsWith(driveName))
+                {
+                    Console.WriteLine($"Default selected drive, type: {driveInfo.DriveType}, free spaxe: {driveInfo.AvailableFreeSpace}, format: {driveInfo.DriveFormat}");
+                }
+            }
+
+            Console.WriteLine("Enter catalogue name:");
+            string catalogueName = Console.ReadLine().Trim();
+
+            string directory = driveName + catalogueName;
+
+            if (Directory.Exists(directory))
+            {
+                Console.WriteLine("Subdirectories:");
+                string[] dirs = Directory.GetDirectories(directory);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Files:");
+                string[] files = Directory.GetFiles(directory);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no such directory!");
+            }
+        }
+    }
+}
